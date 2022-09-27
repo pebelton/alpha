6045514948af63bedb015ea78dd528bd7120be57
pebelton <114211891+pebelton@users.noreply.github.com>
2022-09-27 14:56:28 +0100

    Update extractSummary.sh

--- .github/scripts/extractSummary.sh
+++ .github/scripts/extractSummary.sh
@@ -1,3 +1,3 @@
-startLineNum="$(grep -n "== SUMMARY ==" ../workflows/runOutput.txt | head -n 1 | cut -d: -f1)"
-endLineNum="$(grep -n "JUnit XML file:" ../workflows/runOutput.txt | head -n 1 | cut -d: -f1)"
-awk 'NR>=startLineNum && NR<=endLineNum' ../workflows/runOutput.txt > ../workflows/summary.txt
+startLineNum="$(grep -n "== SUMMARY ==" .../../runOutput.txt | head -n 1 | cut -d: -f1)"
+endLineNum="$(grep -n "JUnit XML file:" ../../runOutput.txt | head -n 1 | cut -d: -f1)"
+awk 'NR>=startLineNum && NR<=endLineNum' ../../runOutput.txt > ../../summary.txt
