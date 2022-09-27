5ecd70256a5f0f62bb670d45fa903eaf52c6ac95
pebelton <114211891+pebelton@users.noreply.github.com>
2022-09-27 16:44:04 +0100

    Update extractSummary.sh

--- .github/scripts/extractSummary.sh
+++ .github/scripts/extractSummary.sh
@@ -1,3 +1,3 @@
-startLineNum="$(grep -n "== SUMMARY ==" runOutput.txt | head -n 1 | cut -d: -f1)"
-endLineNum="$(grep -n "JUnit XML file:" runOutput.txt | head -n 1 | cut -d: -f1)"
-awk 'NR>=startLineNum && NR<=endLineNum' runOutput.txt > summary.txt
+startLineNum="$(grep -n "== SUMMARY ==" ../../runOutput.txt | head -n 1 | cut -d: -f1)"
+endLineNum="$(grep -n "JUnit XML file:" ../../runOutput.txt | head -n 1 | cut -d: -f1)"
+awk 'NR>=startLineNum && NR<=endLineNum' ../../runOutput.txt > summary.txt
